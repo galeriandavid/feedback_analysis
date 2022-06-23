e2f19a960bd4bdbc92bcbc8ee33e9e3d3604e8f9
David Arustamian <david_arustamian@epam.com>
2022-06-23 12:32:07 +0300

    added n\a class for positive reviews

--- app.py
+++ app.py
@@ -12,10 +12,14 @@ text = st.text_input('Enter your feedback')
 
 if st.button('Submit'):
     sentiment, prob, cluster = analyser.predict(text)
-    if sentiment == 'POSITIVE' and prob < 0.9:
-        sentiment = 'NEGATIVE'
-    st.write('Feedback sentiment:')
-    st.write(sentiment.lower())
-    st.write('Feedback type:')
-    st.write(cluster)
+    if sentiment == 'POSITIVE' and prob > 0.9:
+        st.write('Feedback sentiment:')
+        st.write('positive')
+        st.write('Feedback type:')
+        st.write('N/A')
+    else:
+        st.write('Feedback sentiment:')
+        st.write('negative')
+        st.write('Feedback type:')
+        st.write(cluster)
 
